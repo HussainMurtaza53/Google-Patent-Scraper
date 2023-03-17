294700d251319a19873bdf1270a75645cc984eb8
HussainMurtaza53 <41267528+HussainMurtaza53@users.noreply.github.com>
2023-03-18 02:07:53 +0500

    Script Updated

--- scraping/google_patent_scraper.py
+++ scraping/google_patent_scraper.py
@@ -30,7 +30,7 @@ class Google_Patent_Scraper():
         self.options.add_argument("--disable-dev-shm-usage")
 #         self.options.add_argument("--no-sandbox")
         
-        self.driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options = self.options)
+        self.driver = webdriver.Chrome(executable_path = '/app/.chromedriver/bin/chromedriver', chrome_options = self.options)
 #         self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=self.options)
         self.search = "+".join(search.split())
         self.url = 'https://patents.google.com/?q=({0})&oq={0}&page={1}'
