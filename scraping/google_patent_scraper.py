f764f2f23eb5278dda87e4b782808ab2c4964ae9
HussainMurtaza53 <41267528+HussainMurtaza53@users.noreply.github.com>
2023-03-20 16:15:32 +0500

    changes in scraper

--- scraping/google_patent_scraper.py
+++ scraping/google_patent_scraper.py
@@ -33,6 +33,7 @@ class Google_Patent_Scraper():
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument(f'user-agent={user_agent}')
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
