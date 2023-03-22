0492c0efa124e0daeba455cf75aad7c85903def8
HussainMurtaza53 <41267528+HussainMurtaza53@users.noreply.github.com>
2023-03-22 17:43:27 +0500

    Views Updated

--- scraping/views.py
+++ scraping/views.py
@@ -23,7 +23,6 @@ def google_patent_results(request):
     #     patents_data = json.load(f)
     
     data = pd.read_excel('./Data/Google_Patents_Data.xlsx')
-    print('\n----------Data-----------\n', data)
     patents_data = []
     for d in range(len(data)):
         dic = {
@@ -43,7 +42,6 @@ def google_patent_results(request):
         }
 
         patents_data.append(dic)
-    print('\n----------Data LS-----------\n', patents_data)
     context = {
         'patents': patents_data,
         'length': len(patents_data),
