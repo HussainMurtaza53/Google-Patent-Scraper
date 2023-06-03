e56ad25ef55658906ebae772e3242a0cf8adc9e2
HussainMurtaza53 <41267528+HussainMurtaza53@users.noreply.github.com>
2023-06-03 18:32:56 +0500

    Updated project

--- google_patent_scraper_project/wsgi.py
+++ google_patent_scraper_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "google_patent_scraper.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "google_patent_scraper_project.settings")
 
 application = get_wsgi_application()
